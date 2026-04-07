5e7ac80e9aaa1e12882a097e800afc7df08a419d
Mr Snow <mark_snow@nbpsnj.net>
2026-04-07 13:05:25 -0400

    test

--- output/fib.py
+++ output/fib.py
@@ -1,13 +1,5 @@
-def fibonacci(n):
-    """Generate first n Fibonacci numbers."""
-    fib_numbers = []
-    a, b = 0, 1
-    for _ in range(n):
-        fib_numbers.append(a)
-        a, b = b, a + b
-    return fib_numbers
-
-if __name__ == "__main__":
-    fib_sequence = fibonacci(10)
-    for num in fib_sequence:
-        print(num)
+a, b = 0, 1
+print(a)
+for _ in range(9):
+    a, b = b, a + b
+    print(a)
